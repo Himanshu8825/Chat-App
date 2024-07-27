7895cbdc03fff16d85fb7d83d0aa418cab7e1ce5
Himanshu Verma <kumarsurajverma6001@gmail.com>
2024-07-27 17:09:12 +0530

    messages schema done

---
+++ backend/models/MessagesModel.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+
+const messagesSchema = new mongoose.Schema({
+  sender: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  reciver: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: false,
+  },
+  messageType: {
+    type: String,
+    enum: ['text', 'file'],
+    required: true,
+  },
+  content: {
+    type: String,
+    required: function () {
+      return this.messageType === 'text';
+    },
+  },
+  fileUrl: {
+    type: String,
+    required: function () {
+      return this.messageType === 'file';
+    },
+  },
+  timeStamp: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Messages = mongoose.model('Messages', messagesSchema);
+
+module.exports = Messages;
