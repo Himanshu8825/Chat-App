b88acd1e8d1f66592f5c85d777780a6a0ab85d4d
Himanshu Verma <kumarsurajverma6001@gmail.com>
2024-07-27 16:58:00 +0530

    Socket Frontend Done

--- backend/socket.js
+++ backend/socket.js
@@ -1,24 +1,23 @@
-const { Server, Socket } = require('socket.io');
+const { Server } = require('socket.io');
 
 const setupSocket = (server) => {
   const io = new Server(server, {
     cors: {
       origin: [process.env.ORIGIN],
       methods: ['GET', 'POST'],
+      credentials: true,
     },
   });
 
   const userSocketMap = new Map();
 
-  const disconnect = (soket) => {
-    console.log(`Client disconnected: ${soket.id}`);
+  const disconnect = (socket) => {
+    console.log(`Client disconnected: ${socket.id}`);
 
     for (const [userId, socketId] of userSocketMap.entries()) {
-      if (socketId === soket.id) {
+      if (socketId === socket.id) {
         userSocketMap.delete(userId);
-        console.log(
-          `User disconnected: ${userId} with socket ID:  ${socketId}`
-        );
+        console.log(`User disconnected: ${userId} with socket ID: ${socketId}`);
         break;
       }
     }
@@ -29,9 +28,9 @@ const setupSocket = (server) => {
 
     if (userId) {
       userSocketMap.set(userId, socket.id);
-      console.log(`User connected : ${userId} with socket ID:  ${socket.id}`);
+      console.log(`User connected: ${userId} with socket ID: ${socket.id}`);
     } else {
-      console.log('User ID not provided during connection. ');
+      console.log('User ID not provided during connection.');
     }
 
     socket.on('disconnect', () => disconnect(socket));
